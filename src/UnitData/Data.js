99a9139b36a603a5cf0c14206a702a1b355c2f7e
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2021-11-27 19:20:26 +0530

    Data Reeder Complete

---
+++ src/UnitData/Data.js
@@ -0,0 +1,19 @@
+import RawData from "./UnitData.json";
+
+let Units = {};
+const keys = RawData[0];
+// The Slice removes the keys
+RawData.slice(1).forEach((RawUnitData) => {
+	let UnitData = {};
+	// This Slice removes the name of the unit
+	RawUnitData.slice(1).forEach((value, index) => {
+		// if we are messing with the armor type then we need to do some special stuff
+		if (keys[index] === "IsLight") UnitData["Armor Type"] = value === true ? "Light" : "Heavy";
+		// else just set the value
+		else UnitData[keys[index]] = value;
+	});
+	// Set the data with name
+	Units[RawUnitData[0]] = UnitData;
+});
+
+export default Units;
