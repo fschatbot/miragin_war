cd10f1bface7f19707d84bf896b859b5e20cf866
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2021-11-27 19:52:07 +0530

    Small Fix

--- src/UnitData/Data.js
+++ src/UnitData/Data.js
@@ -8,9 +8,9 @@ RawData.slice(1).forEach((RawUnitData) => {
 	// This Slice removes the name of the unit
 	RawUnitData.slice(1).forEach((value, index) => {
 		// if we are messing with the armor type then we need to do some special stuff
-		if (keys[index] === "IsLight") UnitData["Armor Type"] = value === true ? "Light" : "Heavy";
+		if (keys[index + 1] === "IsLight") UnitData["Armor Type"] = value === true ? "Light" : "Heavy";
 		// else just set the value
-		else UnitData[keys[index]] = value;
+		else UnitData[keys[index + 1]] = value;
 	});
 	// Set the data with name
 	Units[RawUnitData[0]] = UnitData;
