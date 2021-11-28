ec89942462754935ca01b4501f7fe9d1cc384226
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2021-11-28 09:05:35 +0530

    Added DarkMode Controll

--- src/index.js
+++ src/index.js
@@ -9,3 +9,12 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 );
+
+if (
+	localStorage.theme === "dark" ||
+	(!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+	document.documentElement.classList.add("dark");
+} else {
+	document.documentElement.classList.remove("dark");
+}
